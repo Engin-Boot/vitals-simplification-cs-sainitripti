6154e8bd4e5b8350439501e147d487d38caca254
Tripti Saini <triptisaini123@gmail.com>
2020-09-09 01:26:06 +0530

    Add different reporters

--- checker.cs
+++ checker.cs
@@ -38,6 +38,44 @@ struct Vital
     }
 }
 
+public class AlertInSMS
+{
+    public void SendAlert(string message)
+    {
+        Console.WriteLine("SMS Alert : " + message);
+    }
+}
+
+public class AlertInSound       // Task
+{
+    public void SendAlert(string message)
+    {
+        Console.WriteLine("Sound Alert : " + message);
+    }
+}
+
+public delegate void Alert(string message);     // Command
+
+public class Thread         // Source
+{
+    Alert _alert;
+
+    public Thread(Alert alert)
+    {
+        this._alert = alert;
+    }
+
+    public void Start(string message)
+    {
+        this._alert.Invoke(message);
+    }
+
+    public void Stop()
+    {
+
+    }
+}
+
 class Checker
 {
     static Vital[] vitals;
@@ -50,31 +88,25 @@ class Checker
             new Vital("RESPRATE", 30, 95) };
     }
 
-    static bool vitalIsOk(float value, Vital vital)
+    static void vitalIsOk(Thread t, float value, Vital vital)
     {
         if(value < vital.LowerLimit)
         {
-            Console.WriteLine(vital.Name + " is low!");
-            return false;
+            string message = vital.Name + " is low!";
+            t.Start(message);
         }
         else if (value > vital.UpperLimit)
         {
-            Console.WriteLine(vital.Name + "is high!");
+            string message = vital.Name + "is high!";
+            t.Start(message);
         }
-        return true;
     }
-    static bool vitalsAreOk(float[] values) {
-        
-        bool allVitalAreOk = true;
-
-        for(int i = 0; i < values.Length; i++)
+    static void vitalsAreOk(Thread t, float[] values)
+    {
+        for (int i = 0; i < values.Length; i++)
         {
-            if(!vitalIsOk(values[i], vitals[i]))
-            {
-                allVitalAreOk = false;
-            }
+            vitalIsOk(t, values[i], vitals[i]);
         }
-        return allVitalAreOk;
     }
     static void ExpectTrue(bool expression) {
         if(!expression) {
@@ -89,12 +121,21 @@ class Checker
         }
     }
     static int Main() {
-        ExpectTrue(vitalsAreOk(new float[]{ 100, 95, 60}));
-        ExpectFalse(vitalsAreOk(new float[]{ 40, 91, 92 }));
 
-        ExpectTrue(vitalIsOk(110, vitals[(int)VitalList.BPM]));
-        ExpectFalse(vitalIsOk(80, vitals[(int)VitalList.SPO2]));
-        ExpectFalse(vitalIsOk(100, vitals[(int)VitalList.RESPRATE]));
+        AlertInSMS smsAlert = new AlertInSMS();
+        Alert _smsAlertCommand = new Alert(smsAlert.SendAlert);
+        Thread _t1 = new Thread(_smsAlertCommand);
+
+        vitalsAreOk(_t1, new float[]{ 100, 95, 60});
+        vitalsAreOk(_t1, new float[]{ 40, 91, 92 });
+
+        AlertInSound soundAlert = new AlertInSound();
+        Alert _soundAlertCommand = new Alert(soundAlert.SendAlert);
+        Thread _t2 = new Thread(_soundAlertCommand);
+        vitalIsOk(_t2, 110, vitals[(int)VitalList.BPM]);
+        vitalIsOk(_t2, 80, vitals[(int)VitalList.SPO2]);
+        vitalIsOk(_t2, 100, vitals[(int)VitalList.RESPRATE]);
+
 
         Console.WriteLine("All ok");
         return 0;
