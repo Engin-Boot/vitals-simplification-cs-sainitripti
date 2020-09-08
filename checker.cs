e0e35e7ee560fd39717caa669edffa3fad6491e4
Tripti Saini <triptisaini123@gmail.com>
2020-09-09 00:16:42 +0530

    Reduce CCN vitalsAreOk

--- checker.cs
+++ checker.cs
@@ -1,17 +1,71 @@
 using System;
 using System.Diagnostics;
 
+enum Vital
+{
+    BPM,
+    SPO2,
+    RESPRATE,
+    VITALCOUNT
+};
+
+struct Limit
+{
+    int lowerLimit;
+    int upperLimit;
+
+    public Limit(int lowerLimit, int upperLimit)
+    {
+        this.lowerLimit = lowerLimit;
+        this.upperLimit = upperLimit;
+    }
+
+    public int LowerLimit
+    {
+        get { return lowerLimit; }
+        set { lowerLimit = value; }
+    }
+
+    public int UpperLimit
+    {
+        get { return upperLimit; }
+        set { upperLimit = value; }
+    }
+}
+
 class Checker
 {
-    static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
+    static Limit[] vitalLimit;
+
+    static Checker()
+    {
+        vitalLimit = new Limit[(int)Vital.VITALCOUNT];
+
+        vitalLimit[(int)Vital.BPM] = new Limit(70, 150);
+        vitalLimit[(int)Vital.SPO2] = new Limit(90, 100);
+        vitalLimit[(int)Vital.RESPRATE] = new Limit(30, 95);
+    }
+
+    static bool vitalIsOk(float value, Limit limit)
+    {
+        if(value < limit.LowerLimit || value > limit.UpperLimit)
+        {
             return false;
         }
         return true;
+    }
+    static bool vitalsAreOk(float[] vitalValue) {
+        
+        bool allVitalAreOk = true;
+
+        for(int i = 0; i < vitalValue.Length; i++)
+        {
+            if(!vitalIsOk(vitalValue[i], vitalLimit[i]))
+            {
+                allVitalAreOk = false;
+            }
+        }
+        return allVitalAreOk;
     }
     static void ExpectTrue(bool expression) {
         if(!expression) {
@@ -26,8 +80,8 @@ class Checker
         }
     }
     static int Main() {
-        ExpectTrue(vitalsAreOk(100, 95, 60));
-        ExpectFalse(vitalsAreOk(40, 91, 92));
+        ExpectTrue(vitalsAreOk(new float[]{ 100, 95, 60}));
+        ExpectFalse(vitalsAreOk(new float[]{ 40, 91, 92 }));
         Console.WriteLine("All ok");
         return 0;
     }
